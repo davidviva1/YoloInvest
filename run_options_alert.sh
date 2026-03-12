57048a9640c938dd9335ae00bd900e8f878e132e
Hayden <wooyanmis@gmail.com>
2026-03-12 05:58:35 +0000

    route intraday option alerts to dedicated group -5289147225

--- run_options_alert.sh
+++ run_options_alert.sh
@@ -26,5 +26,8 @@ for var_name in "${required_vars[@]}"; do
   fi
 done
 
+# Override: send intraday alerts to the dedicated alert group
+export TELEGRAM_CHAT_ID="-5289147225"
+
 pip install -r requirements.txt >/tmp/yoloinvest-options-alert-pip.log 2>&1
 python3 -m yoloinvest.options_alert.alert
